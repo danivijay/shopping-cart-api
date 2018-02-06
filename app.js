fb4520b77017aa3cf6ea4c61a4813fef0ae8aa06
Dani Vijay <danimvijay@gmail.com>
2018-02-06 10:54:57 +0530

    add products get

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const app = express()
 
-app.use((req, res, next) => {
-  res.status(200).json({
-    message: 'hello world'
-  })
-})
+const productsRoutes = require('./routes/products')
+
+app.use('./products', productsRoutes)
 
 module.exports = app
